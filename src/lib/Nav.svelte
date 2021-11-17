2b78083f61c03f1334eec6d1db1f6fa61a34f8a9
Felipe Villegas <felavid@gmail.com>
2021-11-16 21:13:41 -0500

    Pages, layout, and nav component

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,34 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        <li><a href="login.php">Login</a></li>
+    </ul>
+</nav>
+
+<style>
+    nav ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #333;
+    }
+
+    nav li {
+        float: left;
+    }
+
+    nav li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+
+    nav li a:hover {
+        background-color: #111;
+    }
+</style>
